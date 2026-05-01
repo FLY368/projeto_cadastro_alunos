a7a01a2bb49ec982fa225a155a4d7ddf8a0816ee
FLY368 <ls444199@gmail.com>
2026-05-01 02:06:33 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# projeto_cadastro_alunos
+Projeto em Python para cadastro de alunos com validação de entradas, cálculo de média e relatório de desempenho.
---
+++ exe.py
@@ -0,0 +1,123 @@
+#essa função classifica se o aluno foi aprovado, reprovado ou se ele está de recuperação.
+def classificar(notas):
+  if notas >= 7:
+    return("aprovado")
+  elif notas >= 5:
+    return("recuperação")
+  else: 
+    return("reprovado")
+#essa função faz a média dos alunos cadastrados.
+def media_alunos(alunos):
+   notas = [aluno["notas"]for aluno in alunos]
+   return sum(notas) / len(notas)
+#lista alunos.
+alunos = []
+#while true deixa tudo verdadeiro para rodar o break
+while True:
+ while True:
+#pede o nome para o usuário
+  nome = input("digite o nome do aluno:")
+  #strip apaga os espaços em branco e se estiver vazio, pede para que o usuario digite novamente seu nome
+  if nome.strip() =="":
+    print("Nome não pode ficar vazio")
+#for char percorre caracter por caracter, isdigit verifica se tem numero e devolve T ou F, "any" identifica se há pelo menos 1 T se sim printa a msg p usuario
+  elif any(char.isdigit() for char in nome):
+    print("Não são permitidos números")
+  else:
+    break
+#while cria o loop e o true deixa sempre verdadeiro 
+ while True: 
+#try tenta executar e se nao der certo vai pro except até o usuário digitar uma idade válida.
+    try:
+#variavel idade atrelada ao input q esta configurado para receber apenar numeros inteiros e o input esta printando para o usuário digitar a idade do próprio.
+        idade = int(input("digite a idade do aluno:"))
+#se idade for menor ou igual a zero, o console printa invalido.
+        if idade <= 0:
+           print("idade inválida, tente novamente")
+# se a idade for maior que 0, o loop é encerrado
+        else:
+           break
+      #se chegou aqui ele vai printar que deu erro e vai começar o loop dnv ate o usuario digitar corretamente,
+    except:
+        print("você digitou algo errado!")
+#while cria um loop e true deixa sempre verdadeiro
+ while True:
+#try tenta executar e se n der certo vai pro except até o usuário digitar um nota válida.
+    try: 
+#notas aceitando apenas numeros entre 0-10
+        notas = float(input("digite a nota do aluno:"))
+# se a nota for menor que 0 ou maior que 10, mostra mensagem de erro e pede novamente
+        if notas < 0 or notas > 10:
+          print("nota inválida, digite a nota de 0-10")
+#se n vida que segue
+        else:
+            break
+#se entrar no except vai printar invalido e comecar o loop dnv
+    except:
+        print("você digitou uma nota inválida!")
+#dicionario "aluno" 
+ aluno = {
+ "nome": nome,
+ "idade": idade,
+ "notas": notas,
+ "situacao": classificar(notas)
+ }
+#add o dicionario "aluno" na ultima posicao da lista.
+ alunos.append(aluno)
+#variavel "continuar" pergunta ao usuario se ele quer cadastrar um novo aluno, ".lower()" deixa a resposta do usuario em minusculo
+ continuar = input("Quer cadastrar um novo aluno? (s/n:) ").lower()
+ # se o usuário NÃO digitar "s", o cadastro é encerrado
+ if continuar != "s":
+  break
+
+#cabecalho
+print("\n ===Lista de alunos===\n")
+#for percorrendo dict aluno dentro da list alunos, bem intuitivo.
+for aluno in alunos:
+   print(f"nome: {aluno['nome']}")
+   print(f"idade: {aluno['idade']}")
+   print(f"notas: {aluno['notas']}")
+   print(f"situação: {aluno['situacao']}")
+   print("\n")
+   print("-" * 25)
+#cabecalho
+print("\n ===Média final da turma===\n")
+#mostra a media dos alunos, puxando a funçao da L10
+print (media_alunos(alunos))
+#cabecalho
+print("\n ===Relatório final===\n")
+#contadores para cada situação
+aprovado = 0
+recuperacao = 0 
+reprovado = 0
+#percorre a lista de alunos e classifica cada um
+for aluno in alunos:
+  resultado = classificar(aluno["notas"])
+#add ao contador de acordo com a situação retornada
+  if resultado == "aprovado":
+    aprovado += 1
+  elif resultado == "recuperação":
+    recuperacao += 1
+  else:
+    reprovado += 1
+#mostra o resultado final
+print("aprovado: ", aprovado)
+print("Em recuperação: ", recuperacao)
+print("reprovados: ", reprovado)
+#usei para pular uma linha 
+print("\n")
+#pega o primeiro aluno da lista como referencia da menor e maior nota
+maior_nota = alunos[0]
+menor_nota = alunos[0]
+#percorre todos os alunos da lista
+for aluno in alunos:
+   if aluno["notas"] > maior_nota["notas"]:
+      maior_nota = aluno #atualiza o aluno com maior nota
+   if aluno["notas"] < menor_nota["notas"]:
+      menor_nota = aluno #atualiza o aluno com menor nota
+#mostra o aluno com maior nota
+print("Maior nota: ",maior_nota["nome"], "=", maior_nota["notas"])
+#mostra o aluno com menor nota
+print("Menor nota: ",menor_nota["nome"], "=", menor_nota["notas"])
+#quebra de linha
+print("\n")
